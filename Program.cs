a4afa3e6378d05789e28fd930a93ee94938a4a77
SrRizzi <carlosadmsnt@hotmail.com>
2023-12-24 00:33:11 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine($"Iniciando Nokia!");
+
+Smartphone nokia = new Nokia("12345", "Nokia", "12345", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Yt Music");
+
+Console.WriteLine();
+
+Console.WriteLine($"Iniciando iPhone!");
+
+Smartphone iphone = new Nokia("67890", "iPhone", "67890", 256);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("LinkedIn");
